559286420a3edc220b48ebe9e74f18f410bf2074
bobosm <157299719+bobosm@users.noreply.github.com>
2026-03-22 21:56:58 +0100

    Add files via upload

---
+++ Stark_LV1/zadatak5_stark/zadatak5_stark/zadatak5_stark.py
@@ -0,0 +1,23 @@
+﻿try:
+    fhand = open(r"C:\Users\Luka\Desktop\LV1\song.txt")
+    brojac = dict()
+
+    for redak in fhand:
+        redak = redak.lower()
+        rijeci = redak.split()
+
+        for rijec in rijeci:
+            brojac[rijec] = brojac.get(rijec, 0) + 1
+
+    fhand.close()
+
+    jedinstveneRijeci = []
+    for kljuc in brojac:
+        if brojac[kljuc] == 1:
+            jedinstveneRijeci.append(kljuc)
+    
+    print(f"Ukupno riječi koje se pojavljuju samo jednom: {len(jedinstveneRijeci)}")
+    print("To su rijeci: ", jedinstveneRijeci)
+
+except FileNotFoundError:
+    print("Datoteka nije pronađena!")
